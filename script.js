3a613e339a52656cf247c8393cadd957fb3f72ef
Luis Monteiro <luis.monteiro@ufpi.edu.br>
2025-04-28 19:19:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// script.js
-
 async function resolver() {
     const inputs = document.querySelectorAll('#tabuleiro input');
     const values = Array.from(inputs).map(input => input.value === '' ? '0' : input.value);
 
-    // Verifica se todos os números de 0 a 8 estão presentes
     const numeros = new Set(values);
     const esperado = new Set(['0','1','2','3','4','5','6','7','8']);
     if (numeros.size !== 9 || ![...esperado].every(n => numeros.has(n))) {
@@ -35,11 +32,52 @@ async function resolver() {
         });
 
         const resultado = await response.json();
-        resposta.textContent = resultado.mensagem;
-        // Aqui você pode chamar uma função para animar os passos
+        const respostaServidor = resultado.resposta;
+
+        const metodo = Object.keys(respostaServidor)[0];
+        const solucao = respostaServidor[metodo].solucao;
+
+        if (solucao && solucao.length > 0) {
+            animarSolucao(solucao);
+
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+
+            resposta.innerHTML = `
+                <b>Método:</b> ${metodo.toUpperCase()}<br>
+                <b>Nós gerados:</b> ${respostaServidor[metodo].nos_gerados}<br>
+                <b>Nós em fronteira:</b> ${respostaServidor[metodo].nos_fronteira}<br>
+                <b>Profundidade da solução:</b> ${respostaServidor[metodo].profundidade_solucao}<br>
+                <b>Profundidade máxima:</b> ${respostaServidor[metodo].profundidade_max}<br>
+                <b>Completo:</b> ${respostaServidor[metodo].completo}<br>
+                <b>Ótimo:</b> ${respostaServidor[metodo].otimo}<br>
+                <b>Admissível:</b> ${respostaServidor[metodo].admissivel}<br>
+            `;
+        } else {
+            resposta.textContent = 'Não foi encontrada solução.';
+        }
 
     } catch (error) {
         resposta.textContent = 'Erro ao resolver o jogo.';
         console.error(error);
     }
+}
+
+function atualizarTabuleiro(estado) {
+    const inputs = document.querySelectorAll('#tabuleiro input');
+    for (let i = 0; i < estado.length; i++) {
+        inputs[i].value = estado[i] === 0 ? '' : estado[i];
+    }
+}
+
+function animarSolucao(solucoes) {
+    let i = 0;
+    const intervalo = setInterval(() => {
+        if (i < solucoes.length) {
+            const estadoAtual = Array.from(solucoes[i]);
+            atualizarTabuleiro(estadoAtual);
+            i++;
+        } else {
+            clearInterval(intervalo);
+        }
+    }, 800);
 }
